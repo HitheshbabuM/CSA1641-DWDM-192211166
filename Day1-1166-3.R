3f39601f3c778c3493c4735354c46130d4d5c6c6
HitheshbabuM <122254177+HitheshbabuM@users.noreply.github.com>
2024-03-13 13:27:22 +0530

    Add files via upload

---
+++ Day1-1166-3.R
@@ -0,0 +1,10 @@
+data <- c(200, 300, 400, 600, 1000)
+
+min_max_normalized <- (data - min(data)) / (max(data) - min(data))
+
+z_score_normalized <- (data - mean(data)) / sd(data)
+
+print("Min-Max Normalization:")
+print(min_max_normalized)
+print("Z-Score Normalization:")
+print(z_score_normalized)
